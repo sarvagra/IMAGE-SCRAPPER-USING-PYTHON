74069dbf048db885573a0e59e45463a8a2cc861d
sarvagra <sarvagras977@gmail.com>
2024-06-27 22:24:43 +0530

    added more functiond

--- 00_imgscrapper.py
+++ 00_imgscrapper.py
@@ -1,6 +1,14 @@
-import requests.py
-from bs4 import BeautifulPoup as bs
+import requests
+from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging 
 
+flipkart_url= "https://www.flipkart.com/search?q="+"macbooks"
+urlclient=uReq(flipkart_url)
+print(urlclient)
+#to get data dump, use urlclient.read()
+flipkart_page=urlclient.read()
 
+flipkart_html=bs(flipkart_page,'html.parser')
+
+#how to create a click
